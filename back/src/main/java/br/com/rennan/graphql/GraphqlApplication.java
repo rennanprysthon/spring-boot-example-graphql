e8ab2e3e53d41fa9914cc10c14a6b507cd0e06d1
Rennan Prysthon <rennan.prysthon@liferay.com>
2022-11-22 11:24:09 -0300

    feat(back): add more authors and books

--- back/src/main/java/br/com/rennan/graphql/GraphqlApplication.java
+++ back/src/main/java/br/com/rennan/graphql/GraphqlApplication.java
@@ -33,8 +33,11 @@ public class GraphqlApplication {
 			bookRepository.saveAll(List.of(
 				Book.builder().title("Game of Thrones").publisher("").author(grrMartin).build(),
 				Book.builder().title("Dance of Dragons").publisher("").author(grrMartin).build(),
+				Book.builder().title("A Clash of Kings").publisher("").author(grrMartin).build(),
 				Book.builder().title("Lord of the rings").publisher("").author(tolkien).build(),
-				Book.builder().title("Hobbit").publisher("").author(tolkien).build()
+				Book.builder().title("Hobbit").publisher("").author(tolkien).build(),
+				Book.builder().title("The Silmarillion").publisher("").author(tolkien).build(),
+				Book.builder().title("Beren and Lúthien").publisher("").author(tolkien).build()
 			));
 		};
 	}
