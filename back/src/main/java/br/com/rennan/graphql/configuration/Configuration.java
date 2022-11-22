5c6bc5bf5f5229d1ace9c2cf34b6207c19d2129e
Rennan Prysthon <rennan.prysthon@liferay.com>
2022-11-22 09:03:25 -0300

    feat(back): cors configuration

---
+++ back/src/main/java/br/com/rennan/graphql/configuration/Configuration.java
@@ -0,0 +1,22 @@
+package br.com.rennan.graphql.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://127.0.0.1:5173");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/graphql/**", config);
+        return new CorsFilter(source);
+    }
+}
